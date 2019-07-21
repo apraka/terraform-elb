85994f0444177caa066d472a668817aac3d2ce47
apraka <42502441+apraka@users.noreply.github.com>
2019-07-21 17:10:32 +0530

    Update linux_vm2.tf

--- linux_vm2.tf
+++ linux_vm2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "linux_vm2" {
-    ami                         = "ami-0cc293023f983ed53"
-    availability_zone           = "eu-central-1c"
+    ami                         = "i-0050bc873f4a9a2e9"
+    availability_zone           = "us-east-1c"
     ebs_optimized               = false
     instance_type               = "t2.micro"
     monitoring                  = false
